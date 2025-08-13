d1e85c26f70b84ee3e7ac52863165756ce5c58a0
William379-u <202824111+William379-u@users.noreply.github.com>
2025-08-13 10:01:00 -0600

    Guia

---
+++ 13-08-2025/ejercios.js
@@ -0,0 +1,20 @@
+import  Readline  from "readline";
+const rl = Readline.createInterface({
+  input: process.stdin,             
+output: process.stdout
+});
+
+// substring
+rl.question(`Ingrese el codigo del pedido( ej:PED-2025-00045): `,(codigo) => {
+const año= codigo.substring(4,8);
+const numero= codigo.substring(9);
+console.log(`El año del pedido es: ${año}`);
+ console.log(`El código del pedido es: ${numero}`);
+
+rl.close();
+});
+
+
+
+
+ 
