4145722ded0ea53b3621c73dd3b6c34c2da9032c
William379-u <202824111+William379-u@users.noreply.github.com>
2025-09-29 10:17:54 -0600

    clase

--- 29-09-2025/ejercicios.js
+++ 29-09-2025/ejercicios.js
@@ -29,35 +29,35 @@ output: process.stdout
 // 1. Solicitar tres nombres al usuario para ser guardados en un array y mostrar luego cada uno
 
 
-// const nombre =[];
+const nombre =[];
 
-// rl.question( "Dime un nombre: " , ( respuesta ) => {
-//     nombre.push(respuesta);
-//     rl.question( "Dime otro nombre: " , ( respuesta2 ) => {
-//         nombre.push(respuesta2);
-//         rl.question( "Dime un nombre más: " , ( respuesta3 ) => {
-//             nombre.push(respuesta3);
-//             console.log(nombre);
-//             rl.close();
-//         });
-//     });
-// });
+rl.question( "Dime un nombre: " , ( respuesta ) => {
+    nombre.push(respuesta);
+    rl.question( "Dime otro nombre: " , ( respuesta2 ) => {
+        nombre.push(respuesta2);
+        rl.question( "Dime un nombre más: " , ( respuesta3 ) => {
+            nombre.push(respuesta3);
+            console.log(nombre);
+            rl.close();
+        });
+    });
+});
 
 // Solicitar al usuario tres números y calcula el promedio
-// const numeros =[];
+const numeros =[];
 
-// rl.question( "Dime un número: " , ( respuesta ) => {
-//     numeros.push(parseFloat(respuesta));
-//     rl.question( "Dime otro número: " , ( respuesta2 ) => {
-//         numeros.push(parseFloat(respuesta2));
-//         rl.question( "Dime un número más: " , ( respuesta3 ) => {
-//             numeros.push(parseFloat(respuesta3));
-//             const promedio = numeros.reduce((acc, curr) => acc + curr, 0) / numeros.length;
-//             console.log("El promedio es: " + promedio);
-//             rl.close();
-//         });
-//     });
-// });
+rl.question( "Dime un número: " , ( respuesta ) => {
+    numeros.push(parseFloat(respuesta));
+    rl.question( "Dime otro número: " , ( respuesta2 ) => {
+        numeros.push(parseFloat(respuesta2));
+        rl.question( "Dime un número más: " , ( respuesta3 ) => {
+            numeros.push(parseFloat(respuesta3));
+            const promedio = numeros.reduce((acc, curr) => acc + curr, 0) / numeros.length;
+            console.log("El promedio es: " + promedio);
+            rl.close();
+        });
+    });
+});
 
 // 3. Solicita al usuario las temperaturas de tres días y muestra cuál fue la más alta
 
