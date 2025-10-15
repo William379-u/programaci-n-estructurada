5c998937e07a2435e4b01dd484358bc4461460f2
William379-u <202824111+William379-u@users.noreply.github.com>
2025-10-14 20:35:55 -0600

    clase

---
+++ 13-10-2025/clase.js
@@ -0,0 +1,8 @@
+const cuadrado = (numero) => numero * numero;
+console.log("El cuadrado de 4 es:", cuadrado(4));
+
+const promedio = (num1, num2, num3) => (num1 + num2 + num3) / 3;
+console.log("El promedio de 4, 6 y 8 es:", promedio(4, 6, 8));
+
+const areaTriangulo = (base, altura) => (base * altura) / 2;
+console.log("El área del triángulo con base 10 y altura 5 es:", areaTriangulo(10, 5));
